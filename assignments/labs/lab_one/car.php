c0e555b7af700f554cb80c95645023ec7b118802
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-01-14 18:39:59 -0500

    messing around with html layout for better formatting of multiple class instances

--- assignments/labs/lab_one/car.php
+++ assignments/labs/lab_one/car.php
@@ -4,18 +4,19 @@
     class Car {
         //Properties should include make, model and year and include a method to return this information. 
         public string $make;//make is going to be a word, therefore string data type
-        public string $model;//model is also a word, therefore string data type
-        public int $year;//year is a number, therefore integer data type
+        public string $model;//model is also a word or word + numbers, therefore string data type
+        public int $year;//year is a number, therefore integer data type, though technically makes no difference here unless you were using said number in calculations.
 
         public function __construct(string $make, string $model, int $year)
         {
-            $this->make = $make;
-            $this->model = $model;
-            $this->year = $year;
+            $this->make = $make;//make property assigned from constructor parameter
+            $this->model = $model;//etc
+            $this->year = $year;//etc
         }
-        public function getCarInfo(): string
+        public function getCarInfo(): string //method to return car information as a string
         {
-            return "Car Make: {$this->make}, Model: {$this->model}, Year: {$this->year}";
+            return "<h3>Vehicle 1</h3><p><b>Car Make:</b> {$this->make}</p><p><b>Model:</b> {$this->model}</p><p><b>Year:</b> {$this->year}</p>";
+            //calls the properties and places them in a paragraph to allow multiple cars to be displayed neatly.
         }
     }
 
--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -3,9 +3,14 @@
     require_once "connect.php";
     require "header.php"; 
     echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
-    require "footer.php";
+    
     include "car.php";
     //Create an instance of the Car class and display its information using the method you created.
     $myCar = new Car("Toyota", "Camry", 2020);
     echo $myCar->getCarInfo();
+    $myCar = new Car("Ford", "Mustang", 1969);
+    echo $myCar->getCarInfo();
+    $myCar = new Car("Honda", "Civic", 2015);
+    echo $myCar->getCarInfo();
+    require "footer.php";
 ?> 
