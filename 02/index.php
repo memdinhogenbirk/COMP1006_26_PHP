bda7bba9a0da89e230be003ea7fa6f86cc13b0f6
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-01-14 15:20:46 -0500

    please work

--- 02/index.php
+++ 02/index.php
@@ -2,15 +2,40 @@
 
 
 //2. Code Commenting 
-
+//inline Commenting
+/* 
+  Multi-line Commenting
+*/
 
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
+<?php
+declare(strict_types=1);
 
 
+$firstName = "Michael";
+$lastName = "Emdin";
+$age = 30;
+$isStudent = true;
+
+echo "<p>Hello, my name is " . $firstName . " " . $lastName . " and I am " . $age . " years old.</p>";
+
+if ($isStudent) {
+    echo "<p>I am a student.</p>";
+} 
+else {
+    echo "<p>I am not a student.</p>";
+}
 //4. Loosely Typed Language Demo
+$num1 = 1; //int
+$num2 = "2";   // string
 
+function add(int $num1, int $num2) : int {
+    return $num1 + $num2;
+}
 
+echo "<p>The sum is: " . add($num1, $num2) . "</p>";
 //5. Strict Types & Types Hints
 
 
 //6. OOP with PHP 
+?>
