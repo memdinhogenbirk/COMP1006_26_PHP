be39d876ea5e80a836d6eca667202d10f96be089
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-02-09 12:53:37 -0500

    basic submission functionality working

--- assignments/labs/lab_four/process.php
+++ assignments/labs/lab_four/process.php
@@ -3,14 +3,23 @@ require "includes/header.php";
 //  TODO: connect to the database 
 require "includes/connect.php";
 //   TODO: Grab form data (no validation or sanitization for this lab)
+$firstName = htmlspecialchars($_POST['first_name']);
+$lastName  = htmlspecialchars($_POST['last_name']);
+$email     = htmlspecialchars($_POST['email']);
 
-/*
-  1. Write an INSERT statement with named placeholders
-  2. Prepare the statement
-  3. Execute the statement with an array of values
-  4.
-
-*/
+  //1. Write an INSERT statement with named placeholders
+$sql = "INSERT INTO subscribers(first_name, last_name, email) VALUES (:first_name, :last_name, :email)";
+  //2. Prepare the statement
+$stmt = $pdo->prepare($sql);
+  //3. Execute the statement with an array of values
+$stmt ->bindParam(':first_name', $firstName);
+$stmt ->bindParam(':last_name', $lastName);
+$stmt ->bindParam(':email', $email);
+$stmt->execute([
+    ':first_name' => $firstName,
+    ':last_name' => $lastName,
+    ':email' => $email
+]);
 
 ?>
 <!DOCTYPE html>
@@ -29,7 +38,7 @@ require "includes/connect.php";
 
         <!-- TODO: Display a confirmation message -->
         <!-- Example: "Thanks, Name! You have been added to our mailing list." -->
-
+        <p>Thank you, <?php echo ($firstName . ' ' . $lastName); ?>! We have added you to our mailing list.</p>
 
         <p class="mt-3">
             <a href="subscribers.php">View Subscribers</a>
