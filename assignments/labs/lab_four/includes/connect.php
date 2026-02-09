9d9366106c0d592e85803648b648591b05dc1d57
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-02-09 12:20:32 -0500

    more setup

---
+++ assignments/labs/lab_four/includes/connect.php
@@ -0,0 +1,18 @@
+<?php 
+$host = "127.0.0.1"; //hostname
+$db = "lab4"; //database name
+$user = "root"; //username
+$password = ""; //password
+
+//points to the database
+$dsn = "mysql:host=$host;dbname=$db;port=3307";
+
+//try to connect, if connected echo a yay!
+try {
+   $pdo = new PDO ($dsn, $user, $password); 
+   $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+//what happens if there is an error connecting 
+catch(PDOException $e) {
+    die("Database connection failed: " . $e->getMessage()); 
+}
