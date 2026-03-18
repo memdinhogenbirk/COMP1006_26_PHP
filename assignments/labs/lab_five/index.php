79d2452c7633692b027d113694dca5e511968c97
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-03-18 19:33:12 -0400

    home page now displays photos of the user who is currently logged in. Everything seems to be working as intended. Will go over to clean up and comment later

--- assignments/labs/lab_five/index.php
+++ assignments/labs/lab_five/index.php
@@ -5,14 +5,29 @@ if(!isset($_SESSION['user_id'])){
     require 'includes/header.php';
     echo'<main>
         <h1>Welcome to PHOTO SLOP</h1>
-        <p>Login or Create an Account to View or Upload Your Photos!</p>
+        <p><a href="login.php">Login</a> or <a href="signup.php">Create an Account</a> to View or Upload Your Photos!</p>
     </main>';
 }
 else{
+    require 'includes/header_admin.php';
     $currentUser = $_SESSION["user_id"];
     $sql = 'SELECT filePath FROM filePaths WHERE user_id = :user_id';
-}
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':user_id'=>$currentUser]);
+    $photos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-?>
+    $sql = 'SELECT username FROM users WHERE id = :user_id LIMIT 1';
+    $stmt2 = $pdo->prepare($sql);
+    $stmt2->execute([':user_id'=>$currentUser]);
+    $userName = $stmt2->fetch();
+    ?>
+    <main>
+        <h1><?= htmlspecialchars($userName['username']);?>'s Photos</h1>
+        <?php if($photos !== [])foreach($photos as $photo):?>
+            <img src='<?= htmlspecialchars($photo['filePath']) ?>'/>
+        <?php endforeach;
+        else echo"<p>You don't have any photos yet. Want to <a href='upload.php'>UPLOAD</a>?</p>";?>
+    </main>
+<?php } ?>
 
 <?php require "includes/footer.php"; ?>
