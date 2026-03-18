d16d467e3a8881c3058528ddf1bc964c38a40580
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-03-18 18:15:46 -0400

    missed some pointless variables

--- assignments/labs/lab_five/upload.php
+++ assignments/labs/lab_five/upload.php
@@ -17,10 +17,7 @@ $success = "";
 // Check if the form was submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // Get and sanitize form values
-    $name = trim(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS));
-    $description = trim(filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS));
-    $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
+    // super global files image variable
     $userImage = $_FILES['user_image'];
     // This will store the image path for the database
     $filePath = null;
