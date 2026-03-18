eac2d9dfe9ba61ece8dd9dfd0161a137ea5ea71c
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-03-18 17:44:00 -0400

    testing creating db tables purely through php

--- assignments/labs/lab_five/createTables.php
+++ assignments/labs/lab_five/createTables.php
@@ -1,3 +1,12 @@
-<?php require 'connect.php'
-    
+<?php require 'connect.php';
+    $sql ="CREATE TABLE filepaths
+    (
+    file_id INT AUTO_INCREMENT PRIMARY KEY,
+    filePath VARCHAR(255)
+    )";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    echo'success';
+    exit;
 ?>
